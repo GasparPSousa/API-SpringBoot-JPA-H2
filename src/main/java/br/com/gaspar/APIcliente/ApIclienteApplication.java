f05a18504af8736a24f326610a6c28a8cac25caa
GasparPSousa <gasparufrj@gmail.com>
2021-11-29 21:06:43 -0300

    Projeto Inicial

---
+++ src/main/java/br/com/gaspar/APIcliente/ApIclienteApplication.java
@@ -0,0 +1,13 @@
+package br.com.gaspar.APIcliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApIclienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApIclienteApplication.class, args);
+	}
+
+}
