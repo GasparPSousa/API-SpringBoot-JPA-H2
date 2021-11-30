8479ecebc3a8b4cf61294b0b78a51c846adff3d5
GasparPSousa <gasparufrj@gmail.com>
2021-11-29 21:56:13 -0300

    Criando a entidade Cliente

---
+++ src/main/java/br/com/gaspar/APIcliente/entity/Cliente.java
@@ -0,0 +1,33 @@
+package br.com.gaspar.APIcliente.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "tb_cliente")
+public class Cliente implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "nome", nullable = false)
+    private String nome;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "cpf")
+    private String cpf;
+
+
+}
