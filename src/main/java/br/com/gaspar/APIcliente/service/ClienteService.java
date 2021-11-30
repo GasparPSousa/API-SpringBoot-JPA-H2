6b8df47c0cb9cbe5971e54652c9020a5b66defa3
GasparPSousa <gasparufrj@gmail.com>
2021-11-29 22:09:09 -0300

    Criando a classe ClienteService

---
+++ src/main/java/br/com/gaspar/APIcliente/service/ClienteService.java
@@ -0,0 +1,34 @@
+package br.com.gaspar.APIcliente.service;
+
+import br.com.gaspar.APIcliente.entity.Cliente;
+import br.com.gaspar.APIcliente.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ClienteService {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    public Cliente salvar(Cliente cliente) {
+        return clienteRepository.save(cliente);
+    }
+
+    public List<Cliente> listarTodosClientes() {
+        return clienteRepository.findAll();
+    }
+
+    public Optional<Cliente> buscarClientePorId(Long id) {
+        return clienteRepository.findById(id);
+    }
+
+    public void removerClientePorId(Long id) {
+        clienteRepository.deleteById(id);
+    }
+
+
+}
